ecb2bb2e75a5feef65b2f74de2c770d27e808c8f
Sc4ramouche <vladislav.kovechenkov@gmail.com>
2020-05-18 00:22:01 +0300

    lab05 completed || CS61A

--- CS/teachyourselfcs/CS61A/lab05/lab05.py
+++ CS/teachyourselfcs/CS61A/lab05/lab05.py
@@ -1,8 +1,9 @@
+from math import sqrt
 LAB_SOURCE_FILE = "lab05.py"
 
 """ Lab 05: Trees and Proj2 Prep """
 
-from math import sqrt
+
 def distance(city1, city2):
     """
     >>> city1 = make_city('city1', 0, 1)
@@ -14,7 +15,10 @@ def distance(city1, city2):
     >>> distance(city3, city4)
     5.0
     """
-    "*** YOUR CODE HERE ***"
+    lat_diff = get_lat(city1) - get_lat(city2)
+    lon_diff = get_lon(city1) - get_lon(city2)
+    return sqrt(lat_diff ** 2 + lon_diff ** 2)
+
 
 def closer_city(lat, lon, city1, city2):
     """
@@ -30,7 +34,9 @@ def closer_city(lat, lon, city1, city2):
     >>> closer_city(41.29, 174.78, bucharest, vienna)
     'Bucharest'
     """
-    "*** YOUR CODE HERE ***"
+    point = make_city('point', lat, lon)
+    return get_name(min(city1, city2, key=lambda city: distance(point, city)))
+
 
 def check_abstraction():
     """
@@ -69,9 +75,10 @@ def make_city(name, lat, lon):
     1
     """
     if change_abstraction.changed:
-        return {"name" : name, "lat" : lat, "lon" : lon}
+        return {"name": name, "lat": lat, "lon": lon}
     else:
         return [name, lat, lon]
+
 
 def get_name(city):
     """
@@ -84,6 +91,7 @@ def get_name(city):
     else:
         return city[0]
 
+
 def get_lat(city):
     """
     >>> city = make_city('Berkeley', 0, 1)
@@ -94,6 +102,7 @@ def get_lat(city):
         return city["lat"]
     else:
         return city[1]
+
 
 def get_lon(city):
     """
@@ -106,8 +115,10 @@ def get_lon(city):
     else:
         return city[2]
 
+
 def change_abstraction(change):
     change_abstraction.changed = change
+
 
 change_abstraction.changed = False
 
@@ -129,7 +140,11 @@ def acorn_finder(t):
     >>> acorn_finder(t)
     True
     """
-    "*** YOUR CODE HERE ***"
+    if label(t) == 'acorn':
+        return True
+    else:
+        return True in [acorn_finder(b) for b in branches(t)]
+
 
 def sprout_leaves(t, vals):
     """Sprout new leaves containing the data in vals at each leaf in
@@ -164,7 +179,11 @@ def sprout_leaves(t, vals):
           1
           2
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return tree(label(t), [tree(v) for v in vals])
+    else:
+        return tree(label(t), [sprout_leaves(b, vals) for b in branches(t)])
+
 
 def add_chars(w1, w2):
     """
@@ -192,7 +211,26 @@ def add_chars(w1, w2):
     ...       ['For', 'While', 'Set', 'SetComp']) # Must use recursion
     True
     """
-    "*** YOUR CODE HERE ***"
+    # staff solution:
+    if not w1:
+        return w2
+    elif w1[0] == w2[0]:
+        return add_chars(w1[1:], w2[1:])
+    return w2[0] + add_chars(w1, w2[1:])
+
+    # my quirky solution:
+    # def adder(i1, i2):
+    #     if i2 >= len(w2):
+    #         return ''
+    #     if i1 >= len(w1):
+    #         return w2[i2:]
+    #     if w1[i1] == w2[i2]:
+    #         return adder(i1 + 1, i2 + 1)
+    #     else:
+    #         return w2[i2] + adder(i1, i2 + 1)
+
+    # return adder(0, 0)
+
 
 def add_trees(t1, t2):
     """
@@ -232,13 +270,16 @@ def add_trees(t1, t2):
     "*** YOUR CODE HERE ***"
 
 # Shakespeare and Dictionaries
+
+
 def build_successors_table(tokens):
     """Return a dictionary: keys are words; values are lists of successors.
 
     >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
     >>> table = build_successors_table(text)
     >>> sorted(table)
-    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch',
+        'eat', 'guys', 'investigate', 'pie', 'to']
     >>> table['to']
     ['investigate', 'eat']
     >>> table['pie']
@@ -250,10 +291,11 @@ def build_successors_table(tokens):
     prev = '.'
     for word in tokens:
         if prev not in table:
-            "*** YOUR CODE HERE ***"
-        "*** YOUR CODE HERE ***"
+            table[prev] = []
+        table[prev] += [word]
         prev = word
     return table
+
 
 def construct_sent(word, table):
     """Prints a random sentence starting with word, sampling from
@@ -268,8 +310,10 @@ def construct_sent(word, table):
     import random
     result = ''
     while word not in ['.', '!', '?']:
-        "*** YOUR CODE HERE ***"
+        result += ' ' + word
+        word = random.choice(table[word])
     return result.strip() + word
+
 
 def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
     """Return the words of Shakespeare's plays as a list."""
@@ -281,28 +325,35 @@ def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com
         shakespeare = urlopen(url)
         return shakespeare.read().decode(encoding='ascii').split()
 
+
 # Uncomment the following two lines
-# tokens = shakespeare_tokens()
-# table = build_successors_table(tokens)
+tokens = shakespeare_tokens()
+table = build_successors_table(tokens)
+
 
 def random_sent():
     import random
     return construct_sent(random.choice(table['.']), table)
 
 # Tree ADT
+
+
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
     for branch in branches:
         assert is_tree(branch), 'branches must be trees'
     return [label] + list(branches)
 
+
 def label(tree):
     """Return the label value of a tree."""
     return tree[0]
 
+
 def branches(tree):
     """Return the list of branches of the given tree."""
     return tree[1:]
+
 
 def is_tree(tree):
     """Returns True if the given tree is a tree, and False otherwise."""
@@ -313,11 +364,13 @@ def is_tree(tree):
             return False
     return True
 
+
 def is_leaf(tree):
     """Returns True if the given tree's list of branches is empty, and False
     otherwise.
     """
     return not branches(tree)
+
 
 def print_tree(t, indent=0):
     """Print a representation of this tree in which each node is
@@ -341,6 +394,7 @@ def print_tree(t, indent=0):
     print('  ' * indent + str(label(t)))
     for b in branches(t):
         print_tree(b, indent + 1)
+
 
 def copy_tree(t):
     """Returns a copy of t. Only for testing purposes.
