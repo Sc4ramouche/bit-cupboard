0221bd2c751a11309bd451c20c0773e7e6df0786
Sc4ramouche <vladislav.kovechenkov@gmail.com>
2020-05-29 05:03:26 +0300

    lab06 completed || CS61A

---
+++ CS/teachyourselfcs/CS61A/lab06/lab06.py
@@ -0,0 +1,25 @@
+
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    inc = 0
+
+    def adder(x):
+        nonlocal inc
+        result = n + x + inc
+        inc += 1
+        return result
+
+    return adder
